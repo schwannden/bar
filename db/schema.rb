707a8675cc5b38947992cf05b30c028b440805ba
schwannden <schwannden@gmail.com>
2015-11-15 00:51:32 +0800

    add organization to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114041834) do
+ActiveRecord::Schema.define(version: 20151114164744) do
 
   create_table "availables", force: :cascade do |t|
     t.string   "filter_type"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20151114041834) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "organization"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
