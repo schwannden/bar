53c00070ee35da697d4a1c2c051b8aeeb649e12f
tnlin <sss950123@gmail.com>
2015-11-14 14:21:30 +0800

    admin init

---
+++ app/controllers/admin/admin_base_controller.rb
@@ -0,0 +1,14 @@
+class Admin::AdminBaseController < ApplicationController
+  before_action :redirect_if_not_admin
+
+  def redirect_if_not_admin
+    if not admin_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  def admin_signed_in?
+    user_signed_in? && current_user.is_admin?
+  end
+
+end
